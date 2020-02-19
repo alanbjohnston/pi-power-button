6e1251019d4fc99509b439c7be35770da50e2cea
alanbjohnston <alan.b.johnston@gmail.com>
2020-02-19 14:48:43 -0500

    faster flashing, off on reboot

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -12,18 +12,28 @@ GPIO.wait_for_edge(26, GPIO.FALLING)
 time.sleep(1)
 if GPIO.input(26):
 	print("sudo reboot -h now")
+	GPIO.setwarnings(False)
+	GPIO.setup(17, GPIO.OUT)
+	GPIO.output(17, 0);
 	subprocess.call(['reboot', '-h', 'now'], shell=False)
 time.sleep(1)
 if GPIO.input(26):
 	print("sudo reboot -h now")
+	GPIO.setwarnings(False)
+	GPIO.setup(17, GPIO.OUT)
+	GPIO.output(17, 0);
 	subprocess.call(['reboot', '-h', 'now'], shell=False)
 else:
 	print("sudo shutdown -h now")
 	GPIO.setwarnings(False)
 	GPIO.setup(17, GPIO.OUT)
 	GPIO.output(17, 0);
-	time.sleep(1);
+	time.sleep(0.25);
 	GPIO.output(17, 1);
-	time.sleep(1);
+	time.sleep(0.25);
+	GPIO.output(17, 0);
+	time.sleep(0.25);
+	GPIO.output(17, 1);
+	time.sleep(0.25);
 	GPIO.output(17, 0);
 	subprocess.call(['shutdown', '-h', 'now'], shell=False)
