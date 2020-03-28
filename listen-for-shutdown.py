fc74f1c92076c17d38ae362ea451953bae78f4fa
alanbjohnston <alan.b.johnston@gmail.com>
2020-03-28 17:32:30 -0400

    added board sensing for Power LED Pin (either 17 or 16)

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -7,33 +7,38 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+if GPIO.read(12) == false:
+	powerPin = 16
+else:
+	powerPin = 17
 GPIO.wait_for_edge(26, GPIO.FALLING)
 
 time.sleep(1)
 if GPIO.input(26):
 	print("sudo reboot -h now")
 	GPIO.setwarnings(False)
-	GPIO.setup(17, GPIO.OUT)
-	GPIO.output(17, 0);
+	GPIO.setup(powerPin, GPIO.OUT)
+	GPIO.output(powerPin, 0);
 	subprocess.call(['reboot', '-h', 'now'], shell=False)
 time.sleep(1)
 if GPIO.input(26):
 	print("sudo reboot -h now")
 	GPIO.setwarnings(False)
-	GPIO.setup(17, GPIO.OUT)
-	GPIO.output(17, 0);
+	GPIO.setup(powerPin, GPIO.OUT)
+	GPIO.output(powerPin, 0);
 	subprocess.call(['reboot', '-h', 'now'], shell=False)
 else:
 	print("sudo shutdown -h now")
 	GPIO.setwarnings(False)
-	GPIO.setup(17, GPIO.OUT)
-	GPIO.output(17, 0);
+	GPIO.setup(powerPin, GPIO.OUT)
+	GPIO.output(powerPin, 0);
 	time.sleep(0.25);
-	GPIO.output(17, 1);
+	GPIO.output(powerPin, 1);
 	time.sleep(0.25);
-	GPIO.output(17, 0);
+	GPIO.output(powerPin, 0);
 	time.sleep(0.25);
-	GPIO.output(17, 1);
+	GPIO.output(powerPin, 1);
 	time.sleep(0.25);
-	GPIO.output(17, 0);
+	GPIO.output(powerPin, 0);
 	subprocess.call(['shutdown', '-h', 'now'], shell=False)
