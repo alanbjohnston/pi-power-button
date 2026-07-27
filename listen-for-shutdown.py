66bb4d27f0e6bc70eb0755b2445e81294e9b2ac0
Alan Johnston <alan.b.johnston@gmail.com>
2026-07-27 16:34:40 -0400

    Update listen-for-shutdown.py up default

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -84,8 +84,8 @@ def change_mode():
 		sleep(0.35)
 	sleep(0.65)
 	if GPIO.input(push_button):
-		print("sudo nmcli connection down Hotspot")
-		os.system("sudo nmcli connection down Hotspot")
+		print("sudo nmcli connection up preconfigured")
+		os.system("sudo nmcli connection up preconfigured")
 		print("sudo shutdown -h now")
 		os.system("echo 'shutdown due to push button!' | wall")
 		GPIO.setwarnings(False)
